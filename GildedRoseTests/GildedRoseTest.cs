185c09593dbac26ecb989b3d9888fe452c6e8f07
Amal Joy <amal.joy@maqta.ae>
2024-06-21 11:44:30 +0400

    nagative test cases added

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -5,16 +5,7 @@ using GildedRoseKata;
 namespace GildedRoseTests;
 
 public class GildedRoseTest
-{
-    [Fact]
-    public void foo()
-    {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
-        app.UpdateQuality();
-        Assert.Equal("foo", Items[0].Name);
-    }
-    
+{    
      [Fact]
         public void UpdateQuality_NormalItem_BeforeSellInDate()
         {
@@ -178,5 +169,78 @@ public class GildedRoseTest
 
             Assert.Equal(9, items[0].SellIn);
             Assert.Equal(0, items[0].Quality);
+        }
+
+//Negative test cases
+        [Fact]
+        public void Quality_ShouldNot_Exceed_MaxQuality()
+        {
+            var items = new List<Item> 
+            { 
+                new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 } 
+            };
+
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(50, items[0].Quality);
+        }
+
+        [Fact]
+        public void Quality_ShouldNot_BeNegative()
+        {
+            var items = new List<Item> 
+            { 
+                new Item { Name = "Normal Item", SellIn = 10, Quality = 0 } 
+            };
+
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(0, items[0].Quality);
+        }
+
+        [Fact]
+        public void Quality_ShouldDropToZero_AfterConcert()
+        {
+            var items = new List<Item> 
+            { 
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 } 
+            };
+
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(0, items[0].Quality);
+        }
+
+        [Fact]
+        public void Invalid_ItemName_Should_NotChangeQuality()
+        {
+            // Arrange
+            var items = new List<Item> 
+            { 
+                new Item { Name = "Invalid Item", SellIn = 5, Quality = 10 } 
+            };
+
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(9, items[0].Quality);
+        }
+
+        [Fact]
+        public void Sulfuras_ShouldNot_ChangeQuality_OrSellIn()
+        {
+            var items = new List<Item> 
+            { 
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 5, Quality = 80 } 
+            };
+
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+            Assert.Equal(80, items[0].Quality);
+            Assert.Equal(5, items[0].SellIn);
         }
 }
